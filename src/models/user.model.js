1899f92e85b11688f5bc20a04ebd88a26441b195
Ugochidev <ugochukwuchioma16@gmail.com>
2022-02-28 12:17:00 +0100

    feat/modified

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -37,7 +37,7 @@ const userSchema = new Schema(
     role: {
       type: String,
       trim: true,
-      enum: ["User"],
+      enum: ["User", "Admin"],
       default: "User",
     },
     blocked: {
